db3555bbfe38c1ea65bc8635654795c3f25320f3
AnantKumbhar <137088674+AnantKumbhar@users.noreply.github.com>
2025-09-28 23:33:04 +0530

    firebase configured api removed

--- src/firebase.js
+++ src/firebase.js
@@ -8,15 +8,7 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCjg2-XZGmN5gVntl8tGVeQP2voEKKYJf8",
-  authDomain: "my-react-app-aa67d.firebaseapp.com",
-  projectId: "my-react-app-aa67d",
-  storageBucket: "my-react-app-aa67d.firebasestorage.app",
-  messagingSenderId: "914681732641",
-  appId: "1:914681732641:web:036bd8d61c9458dd348d60",
-  measurementId: "G-D4B9EC9ELY"
-};
+//Your configure code here
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
